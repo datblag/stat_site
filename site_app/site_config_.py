50137ec989f863a4823064f040fb3f5abee52a4c
an <d@mail.ru>
2020-08-01 20:02:11 +0900

    set config file

---
+++ site_app/site_config_.py
@@ -0,0 +1,18 @@
+# rename to site_config.py and select options
+
+SERVER_DEVELOP_MODE = 'DEVELOP'
+SERVER_WORK_MODE = 'WORK'
+STAT_PATH = r'path_to_dbf_stat'
+FLASKY_POSTS_PER_PAGE = 50
+
+# server_run_mode = SERVER_DEVELOP_MODE
+
+# SQLALCHEMY_DATABASE_URI = r'sqlite:///path_to_sqlite_file'
+
+
+# POSTGRES_URL = "server_name:port_number"
+# POSTGRES_USER = "user_name"
+# POSTGRES_PW = "password"
+# POSTGRES_DB = "db_name"
+# SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+
