f811c0e4b55eb3dbe2f096fe9f3939dc910314e2
Dudenko_AT <datblag@gmail.com>
2020-09-14 13:47:17 +0900

    add experts columns

--- site_app/routes/reports/main_reports_routes.py
+++ site_app/routes/reports/main_reports_routes.py
@@ -5,6 +5,7 @@ from site_app.template_render import TemplateRender
 from site_app.models import DefectList
 import os
 import uuid
+import logging
 
 
 @app.route('/reports/', methods=['GET'])
@@ -25,14 +26,15 @@ def smo_expert_defects():
     tr.add_titles_row([['№ карты', 10], ['Фамилия', 20], ['Имя', 20], ['Отчество', 20], ['ДР', 20],
                        ['Врач', 20], ['Отделение', 20], ['МКБ10', 20], ['Период', 20], ['', 20], ['Код дефекта', 20],
                        ['Описание дефекта', 20], ['Стоимость услуги', 20], ['Не полежит оплате', 20], ['Всего', 20],
-                       ['Штраф', 20]])
+                       ['Штраф', 20], ['Дата экспертизы', 20], ['Эксперт', 20], ['Акт', 20]])
 
     for defect in query:
+        logging.warning([defect, defect.doctor])
         tr.add_data_row([[defect.patient.num, defect.patient.fam, defect.patient.im, defect.patient.ot,
                           defect.patient.birthday, defect.doctor.doctor_name, defect.doctor.otdel.otdel_name,
                           defect.disease, defect.period_begin, defect.period_end, defect.error_list,
                           defect.error_comment, defect.sum_service, defect.sum_no_pay, defect.get_sum_total(),
-                          defect.sum_penalty]])
+                          defect.sum_penalty, defect.expert_date, defect.expert_name, defect.expert_act_number]])
 
     tr.close_template_file()
     return send_file(os.path.join('files', file_name), as_attachment=True, mimetype='application/vnd.ms-excel',
