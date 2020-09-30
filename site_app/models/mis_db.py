3ad469a77109afde78ad859737fd168f7df5d262
Dudenko_AT <datblag@gmail.com>
2020-09-30 18:15:30 +0900

    get reestr parameters

--- site_app/models/mis_db.py
+++ site_app/models/mis_db.py
@@ -2,7 +2,8 @@ from site_app.site_config import sql_database_mis
 from sqlalchemy import create_engine, MetaData, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
-from sqlalchemy import Column, VARCHAR, INT, DATE
+from sqlalchemy import Column, VARCHAR, INT, DATE, func
+from sqlalchemy.dialects.mssql import UNIQUEIDENTIFIER
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
@@ -64,3 +65,33 @@ class OmsKlDdServiceTable(BaseMis):
     @hybrid_property
     def simple(self):
         return 100
+
+
+# отчетный период
+class HltReportPeriod(BaseMis):
+    __tablename__ = 'hlt_reportperiod'
+    reportperiodid = Column(INT, primary_key=True)
+    uguid = Column(UNIQUEIDENTIFIER())
+
+
+# настройки пользователей
+class XUserSettings(BaseMis):
+    __tablename__ = 'x_usersettings'
+    usersettingid = Column(INT, primary_key=True)
+    valuestr = Column(VARCHAR(500))
+    property = Column(VARCHAR(50))
+
+    def get_ogrn(self):
+        return session_mis.query(XUserSettings).filter(XUserSettings.property == 'ОГРН поликлиники').all()[0].valuestr
+
+
+# справочник ЛПУ
+class OmsLpu(BaseMis):
+    __tablename__ = 'oms_lpu'
+    lpuid = Column(INT, primary_key=True)
+    c_ogrn = Column(VARCHAR(15))
+    stlpu = Column(VARCHAR(1))
+
+    def get_lpuid(self, c_ogrn):
+        select = session_mis.query(func.isnull(func.min(OmsLpu.lpuid), 0).label('lpuid'))
+        return select.filter(OmsLpu.c_ogrn == c_ogrn).filter(OmsLpu.stlpu == '1').all()[0].lpuid
--- site_app/reestr/create_mis_reestr.py
+++ site_app/reestr/create_mis_reestr.py
@@ -1,17 +1,42 @@
 from site_app.models.mis_db import *
 import logging
-
+import datetime
+from sqlalchemy import func
 
 def main():
+
+    date = datetime.date(2020, 7, 23)  # поздноподанные с даты
+    datebegin = datetime.date(2020, 8, 23)
+    dateend = datetime.date(2020, 9, 22)
+    reg_type = 0  # 0 - основной, 1 - ДД, 2 - ВМП, 3 - ОНКО
+    dd = 0
+    if reg_type == 1:
+        dd = 1
+
+    hmp = 0
+    if reg_type == 2:
+        hmp = 1
+
+    onco = 0
+    if reg_type == 3:
+        onco = 1
+
+    c_ogrn = XUserSettings().get_ogrn()
+    logging.warning(['ОГРН', c_ogrn])
+
+    lpuid = OmsLpu.get_lpuid(c_ogrn)
+
+    logging.warning(lpuid)
+
     tmp_department = session_mis.query(OmsDepartmentTable.departmentid, OmsDepartmentTable.rf_lpuid).\
         filter(OmsDepartmentTable.rf_kl_departmenttypeid == 3)
 
     # OmsDepartmentTable.department_list(OmsDepartmentTable, session_mis)
 
-    rows = session_mis.query(HltTapTable, OmsKlDdServiceTable).join(OmsKlDdServiceTable,
-                                               HltTapTable.rf_kl_DDServiceID == OmsKlDdServiceTable.kl_ddserviceid).filter(OmsKlDdServiceTable.simple == 100).limit(1000).all()
-    for row in rows:
-        logging.warning(row)
+    # rows = session_mis.query(HltTapTable, OmsKlDdServiceTable).join(OmsKlDdServiceTable,
+    #                                            HltTapTable.rf_kl_DDServiceID == OmsKlDdServiceTable.kl_ddserviceid).filter(OmsKlDdServiceTable.simple == 100).limit(10).all()
+    # for row in rows:
+    #     logging.warning(row)
 
 
 if __name__ == '__main__':
