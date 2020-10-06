cf3cbbd4b416405cfb12f21c0174003bbeac9edb
Dudenko_AT <datblag@gmail.com>
2020-10-06 16:42:17 +0900

    control patients by excel

---
+++ site_app/excel.py
@@ -0,0 +1,84 @@
+import openpyxl
+from openpyxl.utils import column_index_from_string
+import os
+import datetime
+from site_app.models.mis_db import HltMkabTable, session_mis
+from sqlalchemy import func
+
+
+def main():
+    path_name = r'S:\20201006_госпитализация'
+    file_name = os.path.join(path_name, 'Госпитализация за СЕНТЯБРЬ 2020.xlsx')
+    start_row = 10
+    start_row = start_row - 1  # нумерация с 0 !!!!!!!!!!!!!!!!!!
+    TYPE_MAPPING_BIRDDAY = 1  # 0 год рождения; 1 дата рождения
+
+
+    fam_col = column_index_from_string('C')
+    dr_col = column_index_from_string('D')
+    result_col_history = column_index_from_string('T')
+
+    find_record = 0
+
+    wb = openpyxl.load_workbook(file_name)
+    ws = wb.active
+
+    if ws.max_column <= result_col_history:
+        ws.cell(1, result_col_history).value = ''
+
+    for row_num, row in enumerate(ws.iter_rows()):
+        if row_num < start_row:
+            continue
+        row_num += 1
+        year_bird = row[dr_col-1].value
+        fam = None
+        im = None
+        ot = None
+        fam_value = row[fam_col-1].value
+        if fam_value is None:
+            continue
+        print(fam_value)
+        fio_list = fam_value.strip().split(' ', 1)
+        if len(fio_list) > 1:
+            fam = fio_list[0]
+            fam_value = fio_list[1]
+            fio_list = fam_value.strip().split(' ', 1)
+            im = fio_list[0]
+            if len(fio_list) > 1:
+                ot = fio_list[1]
+        else:
+            continue
+
+        print(fam, im, ot)
+
+        query = session_mis.query(HltMkabTable.num)
+        query = query.filter(func.rtrim(func.ltrim(HltMkabTable.family)) == fam.strip())
+        query = query.filter(func.rtrim(func.ltrim(HltMkabTable.name)) == im.strip())
+        if ot is not None:
+            query = query.filter(func.rtrim(func.ltrim(HltMkabTable.ot)) == ot.strip())
+
+        if TYPE_MAPPING_BIRDDAY == 0:
+            query = query.filter(HltMkabTable.date_bd >= datetime.date(year=year_bird, month=1, day=1))
+            query = query.filter(HltMkabTable.date_bd <= datetime.date(year=year_bird, month=12, day=31))
+        elif TYPE_MAPPING_BIRDDAY == 1:
+            query = query.filter(HltMkabTable.date_bd == year_bird)
+        recs = query.all()
+        print(recs)
+        if recs:
+            find_record += 1
+            # print(row[result_col_history].value)
+            ws.cell(row_num, result_col_history).value = recs[0].num
+            # row[result_col_history].value = recs[0].num
+
+
+        # print(fam.split(' '),year_bird, datetime.date(year=year_bird, month=1, day=1))
+        # for col_num, cel in enumerate(row):
+        #     if TYPE_MAPPING == 0:
+        #         if col_num == fam_col or dr_col == col_num:
+        #             print(cel.value)
+    print(find_record)
+    wb.save(file_name)
+
+if __name__ == '__main__':
+    main()
+
